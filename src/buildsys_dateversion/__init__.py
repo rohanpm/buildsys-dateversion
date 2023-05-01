0a020090aa83bf102faabd3fadc736cd0fadc0b9
Rohan McGovern <rohan@mcgovern.id.au>
2023-05-02 09:47:43 +1000

    Introduce pre-commit hooks

--- src/buildsys_dateversion/__init__.py
+++ src/buildsys_dateversion/__init__.py
@@ -1,5 +1,5 @@
 from ._hooks import build_sdist, build_wheel, build_editable
 
-__version__ = '0'
+__version__ = "0"
 
-__all__ = ['build_sdist', 'build_wheel', 'build_editable']
+__all__ = ["build_sdist", "build_wheel", "build_editable"]
