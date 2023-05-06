d3feffe250658c18fcc4829e6ecb2f2d1ec9a732
Rohan McGovern <rohan@mcgovern.id.au>
2023-05-06 13:22:22 +1000

    Update all pre-commit hooks

--- src/buildsys_dateversion/__init__.py
+++ src/buildsys_dateversion/__init__.py
@@ -1,7 +1,7 @@
 from ._hooks import (
+    build_editable,
     build_sdist,
     build_wheel,
-    build_editable,
     get_requires_for_build_editable,
     get_requires_for_build_sdist,
     get_requires_for_build_wheel,
